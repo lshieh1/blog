9f1ff419acacbc5b0f12f9096a141cbeeaaf8394
TomChoe <doodeitstom@yahoo.com>
2018-04-06 14:37:30 -0400

    CRD app complete

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -10,7 +10,45 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+use App\Blog;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
-    return view('welcome');
+    $blogs = Blog::orderBy('created_at', 'asc')->get();
+
+    return view('blogs', [
+    	'blogs' => $blogs
+    ]);
+});
+
+// Route::get('/blogs', function () {
+// 	return view('blogs');
+// });
+
+Route::post('/blogs', function (Request $request) {
+	$validator = Validator::make($request->all(), [
+		'name' => 'required|max:255',
+	]);
+
+	if ($validator->fails()) {
+		return redirect('/')
+			->withInput()
+			->withErrors($validator);
+	}
+
+	$blog = new Blog;
+	$blog->name = $request->name;
+	$blog->save();
+
+	return redirect('/');
+});
+
+// Route::get('/blogs/{id}', function ($id) {
+// 	//
+// });
+
+Route::delete('/blogs/{id}', function ($id) {
+	Blog::findOrFail($id)->delete();
+
+	return redirect('/');
 });
